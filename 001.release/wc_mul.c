16b1ec87954ada7e7d5947cc948a7a780afd3303
Olin Cordell <olincordell@gmail.com>
2025-08-31 23:53:54 -0400

    Added multiprocess functionality

--- 001.release/wc_mul.c
+++ 001.release/wc_mul.c
@@ -6,9 +6,12 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <sys/wait.h>
+#include <unistd.h>
 
 #define MAX_PROC 100
 #define MAX_FORK 1000
+#define READ_END 0
+#define WRITE_END 1
 
 typedef struct count_t {
 	int linecount;
@@ -16,13 +19,13 @@ typedef struct count_t {
 	int charcount;
 } count_t;
 
-/*
+
 typedef struct plist_t {
 	int pid;
 	int offset;
 	int pipefd[2];
 } plist_t;
-*/
+
 
 int CRASH = 0;
 
@@ -44,6 +47,7 @@ count_t word_count(FILE* fp, long offset, long size)
 	}
 
 	while ((ch=getc(fp)) != EOF && rbytes < size) {
+
 		// Increment character count if NOT new line or space
 		if (ch != ' ' && ch != '\n') { ++count.charcount; }
 
@@ -70,7 +74,7 @@ int main(int argc, char **argv)
 	long fsize;
 	FILE *fp;
 	int numJobs;
-	//plist_t plist[MAX_PROC];
+	plist_t plist[MAX_PROC];
 	count_t total, count;
 	int i, pid, status;
 	int nFork = 0;
@@ -87,7 +91,7 @@ int main(int argc, char **argv)
 	}
 	printf("CRASH RATE: %d\n", CRASH);
 
-
+	// number of child processes to fork
 	numJobs = atoi(argv[1]);
 	if(numJobs > MAX_PROC) numJobs = MAX_PROC;
 
@@ -106,39 +110,82 @@ int main(int argc, char **argv)
 
 	fseek(fp, 0L, SEEK_END);
 	fsize = ftell(fp);
-
 	fclose(fp);
-	// calculate file offset and size to read for each child
+
+	/**
+		get number of processes to fork, fork that many times
+		each child gets its own portion of the file to process
+		once complete, pipes the result to the parent process
+		parent collects all process results and compiles them
+	*/
+
+	long base_chunk_size = fsize/numJobs;
+	long remainder = fsize%numJobs; // handle division issue ******
+	long current_offset = 0;
 
 	for(i = 0; i < numJobs; i++) {
-		//set pipe;
+		
+		long current_chunk_size = base_chunk_size;
+		plist[i].offset = current_offset;
+		
+		if (pipe(plist[i].pipefd) == -1) {
+			fprintf(stderr, "Fork Failed");
+			return 1;
+		}
 
 		if(nFork++ > MAX_FORK) return 0;
 
-		pid = fork();
-		if(pid < 0) {
+		if((plist[i].pid = fork()) < 0) {
 			printf("Fork failed.\n");
-		} else if(pid == 0) {
-			// Child
+		} else if(plist[i].pid == 0) {
+
+			long current_chunk_size = base_chunk_size;
+			if (i < remainder) current_chunk_size++;
+			
 			fp = fopen(argv[2], "r");
-			count = word_count(fp, 0, fsize);
-			// send the result to the parent through pipe
+			count = word_count(fp, plist[i].offset, current_chunk_size);
+			
+			close(plist[i].pipefd[READ_END]); 
+			write(plist[i].pipefd[WRITE_END], &count, sizeof(count));
+
 			fclose(fp);
+			close(plist[i].pipefd[WRITE_END]); 
+
 			return 0;
+		} else if (plist[i].pid > 0) {
+			close(plist[i].pipefd[WRITE_END]);
+			current_offset += current_chunk_size;
 		}
 	}
 
-	// Parent
-	// wait for all children
-	// check their exit status
-	// read the result of normalliy terminated child
-	// re-crete new child if there is one or more failed child
-	// close pipe
+	while (nFork > 0) {
+
+		if ((pid = waitpid(-1, &status, 0)) > 0) {
+
+			for (i = 0; i < numJobs; i++) {
+				if (plist[i].pid == pid) {
+					count_t temp;
+					read(plist[i].pipefd[READ_END], &temp, sizeof(temp));
+
+					total.linecount += temp.linecount;
+					total.wordcount += temp.wordcount;
+					total.charcount += temp.charcount;
+
+					close(plist[i].pipefd[READ_END]);
+					nFork--;
+					break;
+				}
+			}
+
+		} else {
+			// error handling
+		}
+	}
 
 	printf("\n========== Final Results ================\n");
-	printf("Total Lines : %d \n", count.linecount);
-	printf("Total Words : %d \n", count.wordcount);
-	printf("Total Characters : %d \n", count.charcount);
+	printf("Total Lines : %d \n", total.linecount);
+	printf("Total Words : %d \n", total.wordcount);
+	printf("Total Characters : %d \n", total.charcount);
 	printf("=========================================\n");
 
 	return(0);
